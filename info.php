e40947ca2eb27f8da6ad114c4748dc98115c9812
Zsolt Munoz <zmunoz@gmail.com>
2018-01-20 15:42:40 -0700

    Added config.json and updated info

--- info.php
+++ info.php
@@ -1,1 +1,15 @@
-<?phpinfo();?>
+<?php
+    $json = file_get_contents("config.json");
+
+    $jsonIterator = new RecusiveIteratorIterator(
+        new RecusiveArrayIterator(json_decode($json, TRUE)),
+        RecusiveIteratorIterator::SELF_FIRST);
+
+    foreach ($jsonIterator as $key => $val) {
+        if(is_array($val)) {
+            echo "$key:\n";
+        } else {
+            echo "$key => $val\n";
+        }
+    }
+?>
