505037ee3b08aa996ffe152e3114ca485cf0acaf
Zsolt Munoz <zmunoz@gmail.com>
2018-01-20 17:15:31 -0700

    Added partials

---
+++ _partials/head.inc.php
@@ -0,0 +1,25 @@
+<?php
+  $json = file_get_contents("/config.json");
+  
+  $jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="description" content="<?php echo $jsonIterator->site->description; ?>">
+<meta name="author" content="<?php echo $jsonIterator->site->author; ?>">
+
+<title>
+  <?php echo $jsonIterator->site->title; ?>
+</title>
+
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/86e82cc8d7.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- Pace -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/white/pace-theme-minimal.min.css">
--- info.php
+++ info.php
@@ -1,14 +1,3 @@
 <?php
-    $json = file_get_contents("config.json");
-
-    $jsonIterator = new RecursiveIteratorIterator(
-        new RecursiveArrayIterator(json_decode($json, TRUE)),
-        RecursiveIteratorIterator::SELF_FIRST);
-
-    foreach ($jsonIterator as $key => $val) {
-        if(is_array($val)) {
-            echo "$key:\n<br/>";
-        } else {
-            echo "$key => $val\n<br/>";
-        }
-    }
+    require("_partials/head.inc.php");
+?>
